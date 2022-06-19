0804916fbd212d0f8c9549cef62ed4aa6225ae2f
Davy Kamanzi <davykamanzi@gmail.com>
2022-06-19 17:30:07 +0300

    Using Layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, matchPath } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Home from "./components/Home/Home";
+import Contributors from "./components/Contributors/Contributors";
+import Tracks from "./components/Tracks/Tracks";
+import Track from "./components/Track/Track";
+import Layout from "./components/Body/Layout";
 
 const App = () => {
-    const match = matchPath({ path: "/", exact: true }, "/");
-    
     return (
         <Router>
-            {!match ? <Nav /> : null}
             <Routes>
                 <Route path="/" exact element={<Home />} />
+                <Route element={<Layout />}>
+                    <Route path="/contributors" element={<Contributors />} />
+                    <Route path="/tracks/:id" element={<Track />} />
+                    <Route path="/tracks" element={<Tracks />} />
+                </Route>
             </Routes>
         </Router>
     );
