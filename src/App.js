8b252b4b268604b24d65d645f39565a2bac442d6
Davy Kamanzi <davykamanzi@gmail.com>
2022-06-19 15:45:35 +0300

    Initialising app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, matchPath } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import Home from "./components/Home/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const match = matchPath({ path: "/", exact: true }, "/");
+    
+    return (
+        <Router>
+            {!match ? <Nav /> : null}
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
