ec8ea495ca2e425b39f78178b54b46a3682c2dbe
Davy Kamanzi <davykamanzi@gmail.com>
2022-06-19 16:14:14 +0300

    Adding contributors component

---
+++ src/components/Contributors/Contributors.js
@@ -0,0 +1,44 @@
+const Contributors = () => {
+    return (
+        <main>
+            <div className="contributors-lead">
+                <p>The making of this album included a crowdfunding campaign that yielded a portion of the funds used to create it. All contributors can claim their respective rewards by <a href="#rewards">clicking here</a>.</p>
+                <p>A big thank you to everyone at the Supporter tier, as well as the following for their financial support:</p>
+            </div>
+            <div className="contributors">
+                <div>
+                    <h2>Bronze</h2>
+                    <ul>
+                        <li>Ruth Nyambura</li>
+                        <li>Bryan Mutai</li>
+                        <li>Sidney Kamanzi</li>
+                        <li>Chizi Muti</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Silver</h2>
+                    <ul>
+                        <li>Mburu Mbugua</li>
+                        <li>Oscar Nzirera</li>
+                        <li>Gichuhi Thuo</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Gold</h2>
+                    <ul>
+                        <li>Eric Lu Savali</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Platinum</h2>
+                    <ul>
+                        <li>Diane Banziririki</li>
+                        <li>Hervé Mugisha</li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Contributors;
