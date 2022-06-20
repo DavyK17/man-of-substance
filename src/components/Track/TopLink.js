05838fa8cfee278f3147ce113c9aa60500306026
Davy Kamanzi <davykamanzi@gmail.com>
2022-06-20 12:32:21 +0300

    Improving track number logic

--- src/components/Track/TopLink.js
+++ src/components/Track/TopLink.js
@@ -1,7 +1,19 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const TopLink = props => {
     let navigate = useNavigate();
+    let params = useParams();
+    const id = parseInt(params.id);
+
+    const changeTrackNum = id => {
+        const input = document.getElementById("track-number");
+        input.value = id;
+    }
+
+    useEffect(() => {
+        changeTrackNum(id);
+    }, [id]);
 
     const handleSubmit = event => {
         event.preventDefault();
@@ -23,7 +35,7 @@ const TopLink = props => {
     return (
         <div className="top-link">
             <form className="track-spinnerbox" onSubmit={handleSubmit}>
-                <input type="number" id="track-number" min="1" max="17" defaultValue={props.current.id}></input>
+                <input type="number" id="track-number" min="1" max="17" defaultValue={id}></input>
             </form>
             <a href="#top">Back to Top</a>
         </div>
