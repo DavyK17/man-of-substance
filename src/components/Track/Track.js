da8e24c9e19d268cc1af3a587d6cf6ccf5521ea8
Davy Kamanzi <davykamanzi@gmail.com>
2022-06-21 03:52:47 +0300

    Removing style function + adding navigation with arrow keys

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import data from "../../assets/data.json";
 import Synopsis from "./Synopsis";
 import Lyrics from "./Lyrics";
@@ -7,7 +8,19 @@ import Credits from "./Credits";
 const Track = props => {
     let { type } = props;
     let params = useParams();
+    let navigate = useNavigate();
     const id = parseInt(params.id);
+
+    useEffect(() => {
+        document.onkeydown = ({ code }) => {
+            if (id === 17 || (id - 1) >= 1) {
+                if (code === "ArrowLeft") return navigate(`/tracks/${id - 1}/${type}`);
+            }
+            if (id === 1 || (id + 1) <= 17) {
+                if (code === "ArrowRight") return navigate(`/tracks/${id + 1}/${type}`);
+            }
+        }
+    }, [id]);
 
     const current = data.tracks.filter(track => parseInt(track.id) === id)[0];
     const previous = data.tracks.filter(track => parseInt(track.id) === id - 1)[0];
@@ -49,15 +62,6 @@ const Track = props => {
         return `${display("min", min)}${and}${display("sec", sec)}`;
     }
 
-    const style = () => {
-        // const arr = current.style.map(el => el.toLowerCase());
-        // const str = arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
-
-        // arr[0] = str;
-        // return arr.join(", ");
-        return current.style.join(" / ")
-    }
-
     const renderBody = type => {
         switch(type) {
             case "synopsis":
@@ -80,7 +84,7 @@ const Track = props => {
                 <h1 className="title">{current.title}</h1>
                 <p className="writers">Written by {writers()}</p>
                 <div className="info">
-                    <p className="style">{style()}</p>
+                    <p className="style">{current.style.join(" / ")}</p>
                     <p><strong>Runtime:</strong><span id="break"></span>{runtime(current.runtime)}</p>
                 </div>
             </header>
