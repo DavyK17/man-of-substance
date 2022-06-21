a5bcc9785c817542384199a90e29d99bb283ab1b
Davy Kamanzi <davykamanzi@gmail.com>
2022-06-21 04:18:45 +0300

    Creating Footer component

---
+++ src/components/Track/Footer.js
@@ -0,0 +1,18 @@
+import Adjacent from "./Adjacent";
+import TopLink from "./TopLink";
+
+const Footer = props => {
+    return (
+        <footer className="track-footer">
+            <div className="previous">
+                <Adjacent type={props.type} seq={-1} previous={props.previous} />
+            </div>
+            <TopLink type={props.type} />
+            <div className="next">
+                <Adjacent type={props.type} seq={1} next={props.next} />
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
