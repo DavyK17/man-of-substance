3cecb18293d5a7acadd9db6538180a92708a2be2
Davy Kamanzi <davykamanzi@gmail.com>
2022-06-21 17:19:01 +0300

    Adding Credits route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/Body/Layout";
 import Home from "./components/Home/Home";
+import Credits from "./components/Credits/Credits";
 import Contributors from "./components/Contributors/Contributors";
 import Tracks from "./components/Tracks/Tracks";
 import Track from "./components/Track/Track";
@@ -17,6 +18,7 @@ const App = () => {
         <Routes>
             <Route path="/" exact element={<Home />} />
             <Route element={<Layout />}>
+                <Route path="/credits" element={<Credits />} />
                 <Route path="/contributors" element={<Contributors />} />
                 <Route path="/tracks/:id/credits" element={<Track type="credits" />} />
                 <Route path="/tracks/:id/lyrics" element={<Track type="lyrics" />} />
