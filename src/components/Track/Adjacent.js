7d1891ff47279ff786aa4116f666f0dc8d4e7593
Davy Kamanzi <davykamanzi@gmail.com>
2022-06-21 03:39:02 +0300

    Replacing div with blank container

--- src/components/Track/Adjacent.js
+++ src/components/Track/Adjacent.js
@@ -3,27 +3,27 @@ import { Link } from "react-router-dom";
 const Adjacent = props => {
     if (props.seq === -1 && props.previous) {
         return (
-            <div className="previous">
+            <>
                 <h2>Previous</h2>
                 <p>
                     <Link to={`/tracks/${props.previous.id}/${props.type}`}>
                         {props.previous.title}
                     </Link>
                 </p>
-            </div>
+            </>
         )
     }
 
     if (props.seq === 1 && props.next) {
         return (
-            <div className="next">
+            <>
                 <h2>Next</h2>
                 <p>
                     <Link to={`/tracks/${props.next.id}/${props.type}`}>
                         {props.next.title}
                     </Link>
                 </p>
-            </div>
+            </>
         )
     }
 }
