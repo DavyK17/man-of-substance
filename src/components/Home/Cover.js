461b8358c9d627f385ee0346d3d2d6638950d964
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-05 17:09:41 +0300

    Creating Cover component to render WebP where possible

---
+++ src/components/Home/Cover.js
@@ -0,0 +1,13 @@
+import png from "../../assets/img/cover.png";
+import webp from "../../assets/img/cover.webp";
+
+const Cover = () => {
+    return (
+        <picture>
+            <source srcset={webp} type="image/webp"></source>
+            <img className="cover-art" src={png} alt="Man of Substance cover art" />
+        </picture>
+    )
+}
+
+export default Cover;
