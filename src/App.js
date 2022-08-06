f4caffbe758654cfcbdfbe010cebad00186e3827
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-06 21:13:44 +0300

    Adding Rewards to router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from "./components/Body/Layout";
 import Home from "./components/Home/Home";
 import Credits from "./components/Credits/Credits";
 import Contributors from "./components/Contributors/Contributors";
+import Rewards from "./components/Rewards/Rewards";
 import Tracks from "./components/Tracks/Tracks";
 import Track from "./components/Track/Track";
 import NotFound from "./components/Body/NotFound";
@@ -41,8 +42,8 @@ const App = () => {
         <Routes>
             <Route path="/" exact element={<Home />} />
             <Route element={<Layout ver={ver} setVer={setVer} />}>
-                <Route path="/credits" element={<Credits />} />
                 <Route path="/contributors" element={<Contributors />} />
+                <Route path="/rewards" element={<Rewards />} />
                 <Route path="/tracks">
                     <Route path=":id/credits" element={<Track type="credits" tracks={tracks} />} />
                     <Route path=":id/lyrics" element={<Track type="lyrics" tracks={tracks} />} />
@@ -50,6 +51,7 @@ const App = () => {
                     <Route path=":id" element={<Track tracks={tracks} />} />
                     <Route path="" element={<Tracks tracks={tracks} setTracks={setTracks} ver={ver} setVer={setVer} />} />
                 </Route>
+                <Route path="/credits" element={<Credits />} />
                 <Route path="*" element={<NotFound />} />
             </Route>
         </Routes>
