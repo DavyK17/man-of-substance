eda3a09e476859fc950e1f0d6525c6b8eaf89ea0
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-06 20:39:20 +0300

    Fixing srcset JSX attribute

--- src/components/Home/Cover.js
+++ src/components/Home/Cover.js
@@ -4,7 +4,7 @@ import webp from "../../assets/img/cover.webp";
 const Cover = () => {
     return (
         <picture>
-            <source srcset={webp} type="image/webp"></source>
+            <source srcSet={webp} type="image/webp"></source>
             <img className="cover-art" src={png} alt="Man of Substance cover art" />
         </picture>
     )
