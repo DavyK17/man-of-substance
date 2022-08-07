500b2a038d1e2568d550db242cd2955e70eeb06a
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-07 23:08:51 +0300

    Adding track download component

--- src/components/Contributors/Info.js
+++ src/components/Contributors/Info.js
@@ -1,9 +1,10 @@
 import Footer from "./Footer";
+import TrackDownload from "./TrackDownload";
 
 const Info = props => {
-    const { contributor, setContributor, setType } = props;
+    const { contributor, setContributor, setType, validUser } = props;
 
-    const tier = amount => {
+    const getTier = amount => {
         if (amount >= 100 && amount <= 999) return "supporter";
         if (amount >= 1000 && amount <= 1999) return "bronze";
         if (amount >= 2000 && amount <= 3499) return "silver";
@@ -12,17 +13,35 @@ const Info = props => {
         if (amount >= 50000) return "executive";
     }
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        const tier = getTier(contributor.amount);
+
+        if (tier === "supporter") console.log(e.target[0].value);
+        if (tier === "bronze" || tier === "silver") {
+            let checked = [];
+
+            document.querySelectorAll("form.rewards-claim input").forEach(input => {
+                if (input.checked) checked.push(input);
+            });
+
+            checked.forEach(input => console.log(input.value));
+        }
+    }
+
     return (
         <>
             <header className="track-head">
                 <h1 className="title">{contributor.name}</h1>
                 <div className="info">
-                    <p className="style">{tier(contributor.amount).charAt(0).toUpperCase() + tier(contributor.amount).slice(1)}</p>
+                    <p className="style">{getTier(contributor.amount).charAt(0).toUpperCase() + getTier(contributor.amount).slice(1)}</p>
                     <p className="writers">{contributor.email}</p>
                 </div>
             </header>
-            
-            <Footer contributor={contributor} setContributor={setContributor} setType={setType} />
+            <form className="rewards-claim" onSubmit={handleSubmit}>
+                <TrackDownload tier={getTier(contributor.amount)} />
+                <Footer setContributor={setContributor} setType={setType} validUser={validUser} />
+            </form>
         </>
     )
 }
