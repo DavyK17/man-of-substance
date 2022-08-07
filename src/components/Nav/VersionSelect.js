45f9a81c874f709b7fdeca234af73180dd0bc0ab
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-07 21:04:31 +0300

    Moving version select to separate component

---
+++ src/components/Nav/VersionSelect.js
@@ -0,0 +1,22 @@
+const VersionSelect = props => {
+    const { setVer, menuToggle } = props;
+
+    const handleChange = ({ target }) => {
+        setVer(target.value);
+        menuToggle(-1);
+    }
+
+    return (
+        <div className="version-select">
+            <label htmlFor="version">Version</label>
+            <select name="version" id="version" onChange={handleChange} defaultValue="full">
+                <option value="base">Base</option>
+                <option value="mixtape">Mixtape</option>
+                <option value="expanded">Expanded</option>
+                <option value="full">Full</option>
+            </select>
+        </div>
+    )
+}
+
+export default VersionSelect;
