0325f2d6ad7e77642fa9dcdc62b91185f9ba4f7e
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-08 08:39:20 +0300

    Adding challenge API calls

--- src/api/Server.js
+++ src/api/Server.js
@@ -26,6 +26,41 @@ const Server = {
         } catch (err) {
             console.log(err);
         }
+    },
+
+    attemptChallenge: async(name, phone, answer) => {
+        try {
+            let ip = await fetch("https://api.ipify.org/");
+            ip = await ip.text();
+
+            const url = `${Server.url}/challenge`;
+            const data = new URLSearchParams({ name, phone, ip, answer });
+
+            let response = await fetch(url, { method: "POST", body: data });
+            if (response.ok) {
+                response = response.text();
+                return response;
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    },
+
+    getAttempts: async() => {
+        try {
+            let ip = await fetch("https://api.ipify.org/");
+            ip = await ip.text();
+
+            const url = `${Server.url}/challenge?ip=${ip}`;
+
+            let response = await fetch(url);
+            if (response.ok) {
+                response = response.json();
+                return response;
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
 
