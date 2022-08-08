68d9f6ed930a5b2ff9ac64fe23441c00356b1f3a
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-08 08:39:13 +0300

    Passing setFound to Result component

--- src/components/Challenge/Challenge.js
+++ src/components/Challenge/Challenge.js
@@ -2,11 +2,12 @@ import { useState } from "react";
 import Start from "./Start";
 import Result from "./Result"
 
-const Challenge = () => {
+const Challenge = props => {
+    const { setFound } = props;
     const [answer, setAnswer] = useState();
     const [started, setStarted] = useState(false);
 
-    if (started) return <Result answer={answer} setStarted={setStarted} />
+    if (started) return <Result answer={answer} setStarted={setStarted} setFound={setFound} />
     return <Start setAnswer={setAnswer} setStarted={setStarted} />
 }
 
