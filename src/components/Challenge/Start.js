c276047ccdbe2c52c1c1f54f1e35f706ff33604e
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-25 00:52:20 +0300

    Adding loading and solved logic

--- src/components/Challenge/Start.js
+++ src/components/Challenge/Start.js
@@ -1,5 +1,7 @@
+import Skeleton from "react-loading-skeleton";
+
 const Start = props => {
-    const { setStarted, setAnswer } = props;
+    const { isLoading, solved, setAnswer, setStarted } = props;
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -9,14 +11,23 @@ const Start = props => {
         }, Math.floor(Math.random() * 3000));
     }
 
-    return (
-        <form className="challenge start" onSubmit={handleSubmit} autoComplete="off">
-            <h1>Secret Challenge</h1>
+    const renderBody = () => {
+        if (isLoading) return <Skeleton />;
+        if (solved) return <p>The challenge has already been solved.</p>;
+        
+        return <>
             <p>How many lines on the album (in total) explicitly reference a fighting game?</p>
             <div className="input challenge">
                 <input type="number" id="challenge-answer" min="1" required />
                 <input type="submit" id="challenge-submit" value="Enter" />
             </div>
+        </>
+    }
+
+    return (
+        <form className="challenge start" onSubmit={handleSubmit} autoComplete="off">
+            <h1>Secret Challenge</h1>
+            {renderBody()}
         </form>
     )
 }
