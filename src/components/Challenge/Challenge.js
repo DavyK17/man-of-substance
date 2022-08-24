ab5764707021e41eb020c69ed24872a840bc6939
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-25 00:51:47 +0300

    Display challenge only if unsolved

--- src/components/Challenge/Challenge.js
+++ src/components/Challenge/Challenge.js
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Server from "../../api/Server";
 import Start from "./Start";
-import Result from "./Result"
+import Result from "./Result";
 
 const Challenge = props => {
     const { setFound } = props;
+    const [isLoading, setIsLoading] = useState(true);
     const [answer, setAnswer] = useState();
     const [started, setStarted] = useState(false);
+    const [solved, setSolved] = useState(false);
 
-    if (started) return <Result answer={answer} setStarted={setStarted} setFound={setFound} />
-    return <Start setAnswer={setAnswer} setStarted={setStarted} />
+    const isSolved = async() => {
+        try {
+            let response = await Server.getCorrectAttempt();
+            if (response) {
+                setSolved(true);
+            }
+            
+            setIsLoading(false);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        isSolved();
+    }, [])
+
+    if (started) return <Result answer={answer} setStarted={setStarted} setSolved={setSolved} setFound={setFound} />
+    return <Start isLoading={isLoading} solved={solved} setAnswer={setAnswer} setStarted={setStarted} />
 }
 
 export default Challenge;
