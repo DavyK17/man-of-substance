786cb840ccb75fd99f8f15d2d80f5d08a1cfa47e
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-25 01:14:45 +0300

    Moving version select to tracklist

--- src/components/Tracks/VersionSelect.js
+++ src/components/Tracks/VersionSelect.js
@@ -1,21 +1,13 @@
-import { useState, useEffect } from "react";
-
 const VersionSelect = props => {
-    const { ver, setVer, menuToggle } = props;
-    const [locked, setLocked] = useState();
-
-    // useEffect(() => {
-    //     setLocked(Math.round(Date.now() / 1000) < 1667509200 ? true : false);
-    // }, []);
+    const { ver, setVer } = props;
 
     const handleChange = ({ target }) => {
         setVer(target.value);
-        menuToggle(-1);
     }
 
-    return locked ? null : (
+    return (
         <div className="version-select">
-            <label htmlFor="version">Version</label>
+            <label htmlFor="version" className="sr-only">Version</label>
             <select name="version" id="version" onChange={handleChange} defaultValue={ver}>
                 <option value="base">Base</option>
                 <option value="mixtape">Mixtape</option>
