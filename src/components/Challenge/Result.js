cdae935b07418a3b4d85c8a3693fa19a1580c4a5
Davy Kamanzi <davykamanzi@gmail.com>
2022-08-25 00:37:53 +0300

    Placing logic in try/catch statement + refining

--- src/components/Challenge/Result.js
+++ src/components/Challenge/Result.js
@@ -1,37 +1,45 @@
 import Server from "../../api/Server";
 
 const Result = props => {
-    const { answer, setStarted, setFound } = props;
+    const { answer, setStarted, setSolved, setFound } = props;
 
     const handleSubmit = async(e) => {
         e.preventDefault();
 
-        let response = await Server.getAttempts();
-        if (response.length >= 2) {
-            let timestamps = response.map(attempt => Date.parse(attempt.createdAt));
-            let unlockTime = Math.max(...timestamps) + 86400000;
-
-            if (unlockTime >= Date.now()) {
-                document.getElementById("status").innerHTML = "You've attempted the challenge enough times today. Come back tomorrow.";
-                setTimeout(() => {
-                    setStarted(false);
-                    setFound(false);
-                }, 3000);
+        try {
+            let response = await Server.getAttempts();
+            if (response.length >= 2) {
+                let timestamps = response.map(attempt => Date.parse(attempt.createdAt));
+                let unlockTime = Math.max(...timestamps) + 86400000;
+    
+                if (unlockTime >= Date.now()) {
+                    document.getElementById("status").innerHTML = "You've attempted the challenge enough times today. Come back tomorrow.";
+                    setTimeout(() => {
+                        setStarted(false);
+                        setFound(false);
+                    }, 3000);
+                }
+                return;
             }
-            return;
+            
+            response = await Server.attemptChallenge(e.target["challenger-name"].value, e.target["challenger-phone"].value, answer);
+            setTimeout(() => {
+                if (answer === parseInt(process.env.REACT_APP_CHALLENGE_ANSWER)) {
+                    document.getElementById("status").innerHTML = "Congratulations! You will receive your prize money shortly.";
+                    setSolved(true);
+                    setTimeout(() => {
+                        setFound(false);
+                    }, 1500);
+                } else {
+                    document.getElementById("status").innerHTML = "Your answer was wrong. Try again.";
+                    setTimeout(() => {
+                        setStarted(false);
+                    }, 1500);
+                }
+            }, Math.floor(Math.random() * 3000));
+        } catch (err) {
+            console.log(err);
         }
-        
-        response = await Server.attemptChallenge(e.target["challenger-name"].value, e.target["challenger-phone"].value, answer);
-        setTimeout(() => {
-            if (answer === parseInt(process.env.REACT_APP_CHALLENGE_ANSWER)) {
-                document.getElementById("status").innerHTML = "Congratulations!";
-            } else {
-                document.getElementById("status").innerHTML = "Your answer was wrong. Try again.";
-                setTimeout(() => {
-                    setStarted(false);
-                }, 1500);
-            }
-        }, Math.floor(Math.random() * 3000));
     }
 
     return (
