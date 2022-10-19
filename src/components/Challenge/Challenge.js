c1b1481d00df0b5167b45a3627ac75a79d344e6b
Davy Kamanzi <davykamanzi@gmail.com>
2022-10-19 16:31:22 +0300

    Removing redundant variables

--- src/components/Challenge/Challenge.js
+++ src/components/Challenge/Challenge.js
@@ -1,34 +1,14 @@
-import { useState, useEffect } from "react";
-import Server from "../../api/Server";
+import { useState } from "react";
 import Start from "./Start";
 import Result from "./Result";
 
 const Challenge = props => {
     const { setFound } = props;
-    const [isLoading, setIsLoading] = useState(true);
     const [answer, setAnswer] = useState();
     const [started, setStarted] = useState(false);
-    const [solved, setSolved] = useState(false);
 
-    const isSolved = async() => {
-        try {
-            let response = await Server.getCorrectAttempt();
-            if (response) {
-                setSolved(true);
-            }
-            
-            setIsLoading(false);
-        } catch (err) {
-            console.log(err);
-        }
-    }
-
-    useEffect(() => {
-        isSolved();
-    }, [])
-
-    if (started) return <Result answer={answer} setStarted={setStarted} setSolved={setSolved} setFound={setFound} />
-    return <Start isLoading={isLoading} solved={solved} setAnswer={setAnswer} setStarted={setStarted} />
+    if (started) return <Result answer={answer} setStarted={setStarted} setFound={setFound} />
+    return <Start setAnswer={setAnswer} setStarted={setStarted} />
 }
 
 export default Challenge;
