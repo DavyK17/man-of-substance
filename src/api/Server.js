33617826f8355ca8f727c80865918a71f6f45bcc
Davy Kamanzi <davykamanzi@gmail.com>
2022-10-22 00:27:06 +0300

    Removing console logs from attemptChallenge

--- src/api/Server.js
+++ src/api/Server.js
@@ -52,9 +52,6 @@ const Server = {
             const data = new URLSearchParams({ name, phone, ip, answer });
 
             let response = await fetch(url, { method: "POST", body: data });
-            console.log(response);
-            let text = await response.text();
-            console.log(text);
             if (response.status === 403) return response.status;
             if (response.ok) {
                 response = response.text();
