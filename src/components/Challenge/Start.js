3204df8f888534c63e0d711ebd29b4d193b485e5
Davy Kamanzi <davykamanzi@gmail.com>
2022-10-27 10:54:34 +0300

    Removing redundant renderBody function

--- src/components/Challenge/Start.js
+++ src/components/Challenge/Start.js
@@ -11,21 +11,15 @@ const Start = props => {
         }, Math.floor(Math.random() * 3000));
     }
 
-    const renderBody = () => {
-        return <>
+    return (
+        <form className="challenge start" onSubmit={handleSubmit} autoComplete="off">
+            <h1>Secret Challenge</h1>
             <p>How many lines on the album (in total) explicitly reference a fighting game?</p>
             <div className="input challenge">
                 <input type="number" id="challenge-answer" min="1" required />
                 <input type="submit" id="challenge-submit" value="Enter" />
             </div>
             <p id="status">NOTICE: By clicking "Enter", you are consenting to the collection and use of your IP address by Ginton Entertainment to keep track of your challenge attempts. If you do not agree to this condition, please do not click "Enter".</p>
-        </>
-    }
-
-    return (
-        <form className="challenge start" onSubmit={handleSubmit} autoComplete="off">
-            <h1>Secret Challenge</h1>
-            {renderBody()}
         </form>
     )
 }
