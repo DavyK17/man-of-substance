b7d448a302cd81f12eb604ec292601e162a7e9d1
Davy Kamanzi <davykamanzi@gmail.com>
2022-11-03 17:45:20 +0300

    Adding time-specific tests

--- src/tests/Home/Home.test.js
+++ src/tests/Home/Home.test.js
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import App from "../../App";
 import userEvent from "@testing-library/user-event";
 
-describe("App", () => {
+describe("Home page", () => {
     beforeEach(() => {
         render(
             <Router>
@@ -13,21 +13,50 @@ describe("App", () => {
         );
     });
 
-    test("loads Contributors page when corresponding button is clicked", () => {
-        userEvent.click(screen.getByText("Contributors"));
-        let contributors = screen.getByTestId("contributors");
-        expect(contributors).toBeInTheDocument();
+    describe("Pre-trailer", () => {
+        beforeAll(() => {
+            jest.useFakeTimers();
+            jest.setSystemTime(new Date(1666828800000));
+        });
+
+        afterAll(() => {
+            jest.runOnlyPendingTimers();
+            jest.useRealTimers();
+        });
+
+        test("shows countdown to trailer", () => {
+            let countdown = screen.getByTestId("countdown");
+            expect(countdown).toBeInTheDocument();
+        });
     });
 
-    test("loads Tracklist page when corresponding button is clicked", () => {
-        userEvent.click(screen.getByText("Tracklist"));
-        let tracklist = screen.getByTestId("tracklist");
-        expect(tracklist).toBeInTheDocument();
-    });
+    describe("Post-trailer", () => {
+        beforeAll(() => {
+            jest.useFakeTimers();
+            jest.setSystemTime(new Date(1666915200000));
+        });
 
-    test("loads Credits page when corresponding button is clicked", () => {
-        userEvent.click(screen.getByText("Credits"));
-        let credits = screen.getByTestId("credits");
-        expect(credits).toBeInTheDocument();
+        afterAll(() => {
+            jest.runOnlyPendingTimers();
+            jest.useRealTimers();
+        });
+
+        test("loads Contributors page when corresponding button is clicked", () => {
+            userEvent.click(screen.getByText("Contributors"));
+            let contributors = screen.getByTestId("contributors");
+            expect(contributors).toBeInTheDocument();
+        });
+    
+        test("loads Tracklist page when corresponding button is clicked", () => {
+            userEvent.click(screen.getByText("Tracklist"));
+            let tracklist = screen.getByTestId("tracklist");
+            expect(tracklist).toBeInTheDocument();
+        });
+    
+        test("loads Credits page when corresponding button is clicked", () => {
+            userEvent.click(screen.getByText("Credits"));
+            let credits = screen.getByTestId("credits");
+            expect(credits).toBeInTheDocument();
+        });
     });
 });
