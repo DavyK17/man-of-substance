3f4870e4f2a1e3b4d5de64ffcbae846d44c633a9
Davy Kamanzi <davykamanzi@gmail.com>
2022-11-15 21:36:16 +0300

    Fixing mock function calls

--- src/tests/Track/TopLink.test.js
+++ src/tests/Track/TopLink.test.js
@@ -5,7 +5,7 @@ test("calls handleSubmit when form is submitted", () => {
     const tracksMock = [1, 2, 3];
     const submitMock = jest.fn();
 
-    const { getByLabelText } = render(<TopLink id={"1"} tracks={tracksMock} handleSubmit={submitMock()} />);
+    const { getByLabelText } = render(<TopLink id={"1"} tracks={tracksMock} handleSubmit={submitMock} />);
     let input = getByLabelText("Track number");
 
     fireEvent.change(input, { target: { value: "2" }});
--- src/tests/Tracks/VersionSelect.test.js
+++ src/tests/Tracks/VersionSelect.test.js
@@ -4,7 +4,7 @@ import VersionSelect from "../../components/Tracks/VersionSelect";
 test("calls handleChange when option is selected", () => {
     const changeMock = jest.fn();
 
-    const { getByLabelText } = render(<VersionSelect handleChange={changeMock()} />);
+    const { getByLabelText } = render(<VersionSelect handleChange={changeMock} />);
     let versionSelect = getByLabelText("Version");
 
     fireEvent.change(versionSelect, { target: { value: "base" }});
