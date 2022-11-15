f4536b644e816f2f1a25525489f89ab33283e286
Davy Kamanzi <davykamanzi@gmail.com>
2022-11-15 20:46:02 +0300

    Passing handler functions down through props

--- src/components/Contributors/Contributors.js
+++ src/components/Contributors/Contributors.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { Auth } from "aws-amplify";
+import moment from "moment";
 import Server from "../../api/Server";
 import Intro from "./Intro";
 import Login from "./Login";
@@ -41,12 +43,57 @@ const Contributors = () => {
 
     const validUser = contributor && Object.keys(contributor).length === 6 && (contributor.hasOwnProperty("id") && typeof contributor.name === "string")  && (contributor.hasOwnProperty("name") && typeof contributor.name === "string") && (contributor.hasOwnProperty("email") && typeof contributor.email === "string") && (contributor.hasOwnProperty("amount") && typeof contributor.amount === "number") && (contributor.hasOwnProperty("rewardsClaimed") && typeof contributor.rewardsClaimed === "boolean") && (contributor.hasOwnProperty("signedIn") && contributor.signedIn);
 
+    const loginClick = e => {
+        e.preventDefault();
+        validUser ? setType("info") : setType("login");
+    }
+
+    const awsSignIn = async () => {
+        const username = process.env.REACT_APP_AWS_USER;
+        const password = process.env.REACT_APP_AWS_PASS;
+
+        try {
+            const user = await Auth.signIn(username, password);
+            if (user.attributes.email === process.env.REACT_APP_AWS_USER) return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+
+    const loginSubmit = async e => {
+        e.preventDefault();
+        if (!e.target["rewards-email"].value || e.target["rewards-email"].value === "") return document.getElementById("status").innerHTML = "Please enter a valid email address";
+
+        document.getElementById("status").innerHTML = "Tulia kiambatasi…";
+
+        let data = await Server.getContributorByEmail(e.target["rewards-email"].value);
+        if (!data) return document.getElementById("status").innerHTML = "This email does not exist in the database";
+        if (data.rewardsClaimed) return document.getElementById("status").innerHTML = "Rewards have already been claimed for this user";
+        if (data.amount) {
+            if (data.amount >= 100 && data.amount <= 1999 && Date.now() < 1667509200000)
+                return document.getElementById("status").innerHTML = `Your rewards will be available ${moment(1667509200000).fromNow()}`;
+            if (data.amount >= 2000 && data.amount <= 49999 && Date.now() < 1667250000000)
+                return document.getElementById("status").innerHTML = `Your rewards will be available ${moment(1667250000000).fromNow()}`;
+            if (data.amount >= 50000 && Date.now() < 1666904400000)
+                return document.getElementById("status").innerHTML = `Your rewards will be available ${moment(1666904400000).fromNow()}`;
+        }
+
+        let signedIn = await awsSignIn();
+        if (!signedIn) return document.getElementById("status").innerHTML = "Login failed. Kindly check your Internet connection and try again.";
+
+        let user = { ...data, signedIn };
+        localStorage.setItem("mos-contributor", JSON.stringify(user));
+        localStorage.setItem("mos-contributor-expiry", Date.now() + 86400);
+        setContributor(JSON.parse(localStorage.getItem("mos-contributor")));
+    }
+
     const renderBody = type => {
         let body;
-        let defaultBody = <Intro contributors={contributors} setType={setType} isLoading={isLoading} error={error} validUser={validUser} />;
+        let defaultBody = <Intro contributors={contributors} setType={setType} isLoading={isLoading} error={error} validUser={validUser} loginClick={loginClick} />;
         switch (type) {
             case "login":
-                body = <Login contributor={contributor} setContributor={setContributor} setType={setType} validUser={validUser} />
+                body = <Login contributor={contributor} setContributor={setContributor} setType={setType} validUser={validUser} handleSubmit={loginSubmit} />
                 break;
             case "info":
                 body = <Info contributor={contributor} setContributor={setContributor} setType={setType} validUser={validUser} />
--- src/components/Contributors/Login.js
+++ src/components/Contributors/Login.js
@@ -1,51 +1,8 @@
 import { useEffect } from "react";
-import { Auth } from "aws-amplify";
-import moment from "moment";
-import Server from "../../api/Server";
 import Footer from "./Footer";
 
 const Login = props => {
-    const { setContributor, setType, validUser } = props;
-
-    const awsSignIn = async () => {
-        const username = process.env.REACT_APP_AWS_USER;
-        const password = process.env.REACT_APP_AWS_PASS;
-
-        try {
-            const user = await Auth.signIn(username, password);
-            if (user.attributes.email === process.env.REACT_APP_AWS_USER) return true;
-        } catch (err) {
-            console.log(err);
-            return false;
-        }
-    }
-
-    const handleSubmit = async e => {
-        e.preventDefault();
-        if (!e.target["rewards-email"].value || e.target["rewards-email"].value === "") return document.getElementById("status").innerHTML = "Please enter a valid email address";
-
-        document.getElementById("status").innerHTML = "Tulia kiambatasi…";
-
-        let data = await Server.getContributorByEmail(e.target["rewards-email"].value);
-        if (!data) return document.getElementById("status").innerHTML = "This email does not exist in the database";
-        if (data.rewardsClaimed) return document.getElementById("status").innerHTML = "Rewards have already been claimed for this user";
-        if (data.amount) {
-            if (data.amount >= 100 && data.amount <= 1999 && Date.now() < 1667509200000)
-                return document.getElementById("status").innerHTML = `Your rewards will be available ${moment(1667509200000).fromNow()}`;
-            if (data.amount >= 2000 && data.amount <= 49999 && Date.now() < 1667250000000)
-                return document.getElementById("status").innerHTML = `Your rewards will be available ${moment(1667250000000).fromNow()}`;
-            if (data.amount >= 50000 && Date.now() < 1666904400000)
-                return document.getElementById("status").innerHTML = `Your rewards will be available ${moment(1666904400000).fromNow()}`;
-        }
-
-        let signedIn = await awsSignIn();
-        if (!signedIn) return document.getElementById("status").innerHTML = "Login failed. Kindly check your Internet connection and try again.";
-
-        let user = { ...data, signedIn };
-        localStorage.setItem("mos-contributor", JSON.stringify(user));
-        localStorage.setItem("mos-contributor-expiry", Date.now() + 86400);
-        setContributor(JSON.parse(localStorage.getItem("mos-contributor")));
-    }
+    const { setType, validUser, handleSubmit } = props;
 
     useEffect(() => {
         if (validUser) setType("info");
