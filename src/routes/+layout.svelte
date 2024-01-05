8ed611f3f59f949ead66f0786b3a0371ef70aef2
Davy Kamanzi <davykamanzi@gmail.com>
2024-01-05 23:30:27 +0300

    Configuring Sass

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import "../reset.css";
+	import "../app.scss";
+</script>
+
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Joan&family=Lexend+Deca&display=swap" rel="stylesheet">
+</svelte:head>
+
+<slot />
