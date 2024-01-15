90133401946fd996e79b642705607536ab16481f
Davy Kamanzi <davykamanzi@gmail.com>
2024-01-15 09:02:23 +0300

    Returning credits object itself

--- src/routes/(content)/credits/+page.ts
+++ src/routes/(content)/credits/+page.ts
@@ -1,6 +1,8 @@
 import type { PageLoad } from "./$types";
+import type { Credits } from "$lib";
+
 import { credits } from "$lib/data.json";
 
-export const load: PageLoad = () => {
-	return { credits };
+export const load: PageLoad = (): Credits => {
+	return credits;
 };
