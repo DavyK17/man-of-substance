c865a16e0bea7da8062baba058bd41d64200211c
Davy Kamanzi <davykamanzi@gmail.com>
2024-01-20 16:11:47 +0300

    Removing AWS Amplify

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,6 @@
 <script>
 	import "../reset.css";
 	import "../app.scss";
-
-	import { Amplify } from "aws-amplify";
-	import amplifyconfig from "../amplifyconfiguration.json";
-
-	Amplify.configure(amplifyconfig);
 </script>
 
 <slot />
