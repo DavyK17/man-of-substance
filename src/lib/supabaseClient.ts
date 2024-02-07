74e38072610cc5d5039ade231f57635837efb736
Davy Kamanzi <davykamanzi@gmail.com>
2024-02-07 20:24:51 +0300

    Updating URL

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,7 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 import { dev } from "$app/environment";
 import { PUBLIC_SUPABASE_KEY } from "$env/static/public";
 
-export const supabase = createClient(
-	dev ? "http://127.0.0.1:54321" : "https://xuykcmyvfzwkiyqinpdo.supabase.co",
-	PUBLIC_SUPABASE_KEY
-);
+const url = dev ? "http://127.0.0.1:54321" : "https://ilcfndcbhwqngudvsjkb.supabase.co";
+export const supabase = createClient(url, PUBLIC_SUPABASE_KEY);
