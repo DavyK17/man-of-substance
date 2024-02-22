aedfc7f5da6b5cb0b69e1168e165a0e270d16b1c
Davy Kamanzi <davykamanzi@gmail.com>
2024-02-22 10:24:39 +0300

    Getting URL from env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 import { dev } from "$app/environment";
-import { PUBLIC_SUPABASE_KEY } from "$env/static/public";
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from "$env/static/public";
 
-const url = dev ? "http://127.0.0.1:54321" : "https://ilcfndcbhwqngudvsjkb.supabase.co";
+const url = dev ? "http://127.0.0.1:54321" : PUBLIC_SUPABASE_URL;
 export const supabase = createClient(url, PUBLIC_SUPABASE_KEY);
