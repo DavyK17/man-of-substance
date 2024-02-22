f44bc7e2680fc5d9a6bfde558f7c8280cb17c4e3
Davy Kamanzi <davykamanzi@gmail.com>
2024-02-22 11:04:38 +0300

    Removing Supabase files

--- supabase/seed.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO contributors (name, email, amount, rewards_claimed)
-VALUES
-    ('Constance Mwaliko', 'constance@email.com', 100, false),
-    ('Jared Omwenga', 'omwenga@email.com', 1400, false),
-    ('Daniel Karuga', 'd.karuga@email.com', 2750, false),
-    ('Phoebe Michaels', 'phoebem@email.com', 4000, false),
-    ('Mohamed Al-Atif', 'malatif@email.com', 15000, false),
-    ('Tracy Malaika', 'tracy@email.com', 62000, false);
