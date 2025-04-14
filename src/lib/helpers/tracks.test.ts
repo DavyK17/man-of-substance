56c7c7d48a85d278502bf628eebfdfbce739c44c
Davy Kamanzi <davykamanzi@gmail.com>
2025-04-15 02:37:21 +0300

    Fixing tests

--- src/lib/helpers/tracks.test.ts
+++ src/lib/helpers/tracks.test.ts
@@ -1,27 +1,25 @@
-// import type { Track } from "$lib/types/general";
+import type { Track } from "$lib/types/general";
 
 import { expect, it } from "vitest";
-import { Page } from "./tracks";
+import { List, Page } from "./tracks";
 
-// const mockTracks = tracks as Track[];
+it("builds tracklist correctly", () => {
+	const tracklist = List.build(List.mockData as Track[], "base");
+	expect(tracklist).toHaveLength(10);
+});
 
-// it("builds tracklist correctly", () => {
-// 	const tracklist = List.build(mockTracks, "base");
-// 	expect(tracklist).toHaveLength(10);
-// });
+it("gets tracklist parts correctly", () => {
+	const tracklist = List.build(List.mockData as Track[], "full");
 
-// it("gets tracklist parts correctly", () => {
-// 	const tracklist = List.build(mockTracks, "full");
+	const one = List.getPart(tracklist, "full", 1);
+	expect(one).toHaveLength(7);
 
-// 	const one = List.getPart(tracklist, "full", 1);
-// 	expect(one).toHaveLength(7);
+	const two = List.getPart(tracklist, "full", 2);
+	expect(two).toHaveLength(7);
 
-// 	const two = List.getPart(tracklist, "full", 2);
-// 	expect(two).toHaveLength(7);
-
-// 	const three = List.getPart(tracklist, "full", 3);
-// 	expect(three).toHaveLength(3);
-// });
+	const three = List.getPart(tracklist, "full", 3);
+	expect(three).toHaveLength(3);
+});
 
 it("formats runtime unit correctly", () => {
 	let formatted = Page.formatRuntimeUnit("min", 3);
@@ -31,15 +29,15 @@ it("formats runtime unit correctly", () => {
 	expect(formatted).toBe("3 seconds");
 });
 
-// it("displays writers correctly", () => {
-// 	const writers = Page.displayWriters(mockTracks[0]);
-// 	expect(writers).toBe("Davy Kamanzi, Ariane Uwizera and Sidney Kamanzi");
-// });
+it("displays writers correctly", () => {
+	const writers = Page.displayWriters(List.mockData[0] as Track);
+	expect(writers).toBe("Davy Kamanzi, Ariane Uwizera and Sidney Kamanzi");
+});
 
-// it("displays runtime correctly", () => {
-// 	const runtime = Page.displayRuntime(mockTracks[0]);
-// 	expect(runtime).toBe("1 minute and 4 seconds");
-// });
+it("displays runtime correctly", () => {
+	const runtime = Page.displayRuntime(List.mockData[0] as Track);
+	expect(runtime).toBe("1 minute and 4 seconds");
+});
 
 it("renders credit title correctly", () => {
 	let title = Page.renderCreditTitle("producers", ["John Doe"]);
