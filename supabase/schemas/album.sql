cc756f47109e894b63ff49cf8a11b2be18af613c
Davy Kamanzi <davykamanzi@gmail.com>
2025-04-14 21:16:03 +0300

    Adding public contributors view

--- supabase/schemas/album.sql
+++ supabase/schemas/album.sql
@@ -38,3 +38,47 @@ create table public.tracks (
 	missing_from tracklist_version[] null,
 	constraint tracks_pkey primary key (id)
 );
+
+create view public.contributor_names as
+select
+	name,
+	case
+		when amount >= 1000
+		and amount <= 1999 then 'bronze'
+		when amount >= 2000
+		and amount <= 3499 then 'silver'
+		when amount >= 3500
+		and amount <= 4999 then 'gold'
+		when amount >= 5000
+		and amount <= 49999 then 'platinum'
+		when amount >= 50000 then 'executive'
+	end as tier
+from
+	public.contributors
+where
+	amount >= 1000;
+
+create policy "Enable read access based on user ID" on "public"."contributors" as permissive for
+select
+	to authenticated using (
+		(
+			select
+				auth.uid ()
+		) = user_id
+	);
+
+create policy "Enable read access for all users" on "public"."credits" as permissive for
+select
+	to public using (true);
+
+create policy "Enable read access for all users" on "public"."tracks" as permissive for
+select
+	to public using (true);
+
+alter table "public"."challenge_attempts" enable row level security;
+
+alter table "public"."contributors" enable row level security;
+
+alter table "public"."credits" enable row level security;
+
+alter table "public"."tracks" enable row level security;
